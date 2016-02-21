62705732d9576b803df651e8af4a043503b6982d
Eric Tsiliacos <erictsiliacos@Erics-MacBook.local>
2016-02-20 23:43:39 -0800

    parse metric

--- src/Parser/MetricParser.hs
+++ src/Parser/MetricParser.hs
@@ -1,9 +1,19 @@
 module Parser.MetricParser where
 
 import Data.ByteString.Char8
+import Data.ByteString.Char8 (unpack)
+import Data.List.Split (splitOn)
 
-data Metric = Metric Int String Float deriving (Eq, Show)
+-- Event format
+-- deployment_name.job_name.index.agent_id.metric_name metric_value UTC_timestamp
+type Name = String
+type Value = Float
+type Timestamp = Int
+data Metric = Metric Timestamp Name Value deriving (Eq, Show)
 
 metricParser :: ByteString -> Metric
-metricParser = undefined
-
+metricParser rawEvent = Metric timestamp name value
+  where event = fmap unpack (split ' ' rawEvent)
+        timestamp = (read (event !! 2) :: Int)
+        name = (Prelude.head . Prelude.reverse) $ splitOn "." (event !! 0)
+        value = (read (event !! 1) :: Float)
--- test/Parser/MetricParserSpec.hs
+++ test/Parser/MetricParserSpec.hs
@@ -10,4 +10,4 @@ spec :: Spec
 spec = do
   describe "responseParser" $ do
     it "returns a Metric parsed from the response" $ do
-      (metricParser $ pack "deployment_name.job_name.index.agent_id.cpu_usage 82.96 1454644228") `shouldBe` (Metric 1454644228 "cpuUsage" 82.96)
+      (metricParser $ pack "deployment_name.job_name.index.agent_id.cpu_usage 82.96 1454644228") `shouldBe` (Metric 1454644228 "cpu_usage" 82.96)
