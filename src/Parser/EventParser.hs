55f93daf044ada8cebcb8aae428345e564617c4a
Eric Tsiliacos and Shatarupa Nandi <pair+erictsiliacos+snandi@gmail.com>
2016-04-04 17:51:54 -0700

    lint and warnings

--- src/Parser/EventParser.hs
+++ src/Parser/EventParser.hs
@@ -1,24 +1,29 @@
 {-# LANGUAGE DeriveGeneric #-}
 module Parser.EventParser where
 
-import Data.ByteString.Char8 (unpack)
-import Data.List.Split (splitOn)
-import GHC.Generics
-import Data.Aeson (ToJSON)
+import           Data.Aeson            (FromJSON, ToJSON, defaultOptions,
+                                        genericToJSON, toJSON)
+import           Data.ByteString.Char8 (unpack)
+import           Data.List.Split       (splitOn)
+import           GHC.Generics
 
-data Event = Event { time :: String,
+data Event = Event { time   :: String,
                      metric :: Metric
                    } deriving (Eq, Show, Generic)
-instance ToJSON Event
+instance FromJSON Event
+instance ToJSON Event where
+  toJSON = genericToJSON  defaultOptions
 
-data Metric = Metric { name :: String,
+data Metric = Metric { name  :: String,
                        value :: Float
                      } deriving (Eq, Show, Generic)
-instance ToJSON Metric
+instance FromJSON Metric
+instance ToJSON Metric where
+  toJSON = genericToJSON  defaultOptions
 
 eventParser :: String -> Event
-eventParser rawEvent = Event (show timestamp) $ (Metric name value)
+eventParser rawEvent = Event (show timestamp) $ Metric name value
   where event = Prelude.words rawEvent
-        timestamp = (read (event !! 2) :: Int)
-        name = (Prelude.head . Prelude.reverse) $ splitOn "." (event !! 0)
-        value = (read (event !! 1) :: Float)
+        timestamp = read (event !! 2) :: Int
+        name = last $ splitOn "." $ head event
+        value = read (event !! 1) :: Float
