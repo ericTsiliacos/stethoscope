0018bc043394be6614c72e1f16a06cae4ba4bdca
Eric Tsiliacos <erictsiliacos@gmail.com>
2016-04-12 16:09:31 -0700

    Extract out CpuDisplay from Main.elm

--- frontend/Main.elm
+++ frontend/Main.elm
@@ -1,55 +1,10 @@
 module Main (..) where
 
-import Html exposing (..)
-import Effects exposing (..)
+import CpuDisplay exposing (init, update, view, Model)
+import Effects exposing (Never)
+import Html exposing (Html)
 import StartApp
-import Html.Attributes exposing (type', placeholder)
-import Html.Events exposing (onClick)
-import Json.Decode exposing (..)
-import Task exposing (..)
-import Http exposing (..)
-
-type Action
-  = NewEvent (Maybe String)
-  | NoOp
-
-update : Action -> Model -> (Model, Effects Action)
-update action model =
-  case action of
-    NewEvent maybeCpu ->
-      ( Model (Maybe.withDefault model.cpu maybeCpu)
-      , Effects.none
-      )
-    NoOp ->
-      ( model
-      , Effects.none
-      )
-
-view : Signal.Address Action -> Model -> Html
-view address model =
-  div [] [
-    h1 [] [ text "Welcome to Stethoscope" ],
-    span [] [ text model.cpu ]
-  ]
-
-getEvent : Effects Action
-getEvent =
-  Http.get eventDecoder "/monitoring"
-  |> Task.map toString
-  |> Task.toMaybe
-  |> Task.map NewEvent
-  |> Effects.task
-
-eventDecoder : Decoder (Float)
-eventDecoder = Json.Decode.at ["metric", "value"] Json.Decode.float
-
-type alias Model = { cpu : String }
-
-init : (Model, Effects Action)
-init =
-  ( Model "--"
-  , getEvent
-  )
+import Task exposing (Task)
 
 app : StartApp.App Model
 app =
