d803d611bbb0d73695e55330b5e82cf7b558ba78
Eric Tsiliacos <erictsiliacos@Erics-MacBook.local>
2016-06-18 00:03:06 -0700

    Run elm format

--- frontend/CpuDisplay.elm
+++ frontend/CpuDisplay.elm
@@ -1,10 +1,9 @@
-module CpuDisplay exposing
-  ( init
-  , update
-  , view
-  )
-
--- where
+module CpuDisplay
+    exposing
+        ( init
+        , update
+        , view
+        )
 
 import Html exposing (Html, div, h1, span, text, button, input, form)
 import Html.Attributes exposing (type', placeholder, value)
@@ -13,97 +12,95 @@ import Http exposing (get, url)
 import Json.Decode exposing (Decoder)
 import Task exposing (Task)
 
-type alias Model =
-  { cpu : String
-  , startTime : String
-  , endTime : String
-  }
 
-init : (Model, Cmd Msg)
+type alias Model =
+    { cpu : String
+    , startTime : String
+    , endTime : String
+    }
+
+
+init : ( Model, Cmd Msg )
 init =
-  ( Model "--" "" ""
-  , getEvent Nothing
-  )
+    ( Model "--" "" ""
+    , getEvent "/monitoring"
+    )
+
 
 type Msg
-  = FetchEventSucceed String
-  | FetchEventFail Http.Error
-  | UpdateStartTime String
-  | UpdateEndTime String
-  | Filter
-  | NoOp
+    = FetchEventSucceed String
+    | FetchEventFail Http.Error
+    | UpdateStartTime String
+    | UpdateEndTime String
+    | Filter
+    | NoOp
+
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update action model =
-  case action of
-    NoOp ->
-      ( model, Cmd.none )
+    case action of
+        NoOp ->
+            ( model, Cmd.none )
 
-    FetchEventSucceed newCpu ->
-      ( { model
-          | cpu = newCpu
-        }
-      , Cmd.none
-      )
+        FetchEventSucceed newCpu ->
+            ( { model
+                | cpu = newCpu
+              }
+            , Cmd.none
+            )
 
-    FetchEventFail _ ->
-      ( model, Cmd.none )
+        FetchEventFail _ ->
+            ( model, Cmd.none )
 
-    Filter ->
-      ( model
-      , getEvent (Just [("start", model.startTime), ("end", model.endTime)])
-      )
+        Filter ->
+            ( model
+            , getEvent <| url "/monitoring" [ ( "start", model.startTime ), ( "end", model.endTime ) ]
+            )
 
-    UpdateStartTime newStartTime ->
-      ( { model
-          | startTime = newStartTime
-        }
-      , Cmd.none
-      )
+        UpdateStartTime newStartTime ->
+            ( { model
+                | startTime = newStartTime
+              }
+            , Cmd.none
+            )
 
-    UpdateEndTime newEndTime ->
-      ( { model
-          | endTime = newEndTime
-        }
-      , Cmd.none
-      )
+        UpdateEndTime newEndTime ->
+            ( { model
+                | endTime = newEndTime
+              }
+            , Cmd.none
+            )
+
 
 view : Model -> Html Msg
 view model =
-  div []
-    [
-      h1 [] [ text "Welcome to Stethoscope" ],
-      span [] [ text model.cpu ],
-      form []
-        [
-          input
-            [ placeholder "Start"
-            , value model.startTime
-            , onInput UpdateStartTime
+    div []
+        [ h1 [] [ text "Welcome to Stethoscope" ]
+        , span [] [ text model.cpu ]
+        , form []
+            [ input
+                [ placeholder "Start"
+                , value model.startTime
+                , onInput UpdateStartTime
+                ]
+                []
+            , input
+                [ placeholder "End"
+                , value model.endTime
+                , onInput UpdateEndTime
+                ]
+                []
+            , button [ type' "button", onClick Filter ]
+                [ text "Filter" ]
             ]
-            [],
-          input
-            [ placeholder "End"
-            , value model.endTime
-            , onInput UpdateEndTime
-            ]
-            [],
-          button
-            [ type' "button", onClick Filter ]
-            [ text "Filter" ]
         ]
-    ]
 
-getEvent : Maybe (List (String, String)) -> Cmd Msg
-getEvent maybeQuery =
-  let monitoringUrl =
-    case maybeQuery of
-      Nothing ->
-        "/monitoring"
-      Just queryParams ->
-        url "/monitoring" queryParams
-  in
-    Task.perform FetchEventFail FetchEventSucceed (get eventDecoder monitoringUrl |> Task.map toString)
+
+getEvent : String -> Cmd Msg
+getEvent url =
+    Task.perform FetchEventFail FetchEventSucceed (get eventDecoder url |> Task.map toString)
+
 
 eventDecoder : Decoder (Float)
-eventDecoder = Json.Decode.at ["metric", "value"] Json.Decode.float
+eventDecoder =
+    Json.Decode.at [ "metric", "value" ] Json.Decode.float
